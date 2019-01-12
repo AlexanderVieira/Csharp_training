0a93c09947aba3dfc3886beb3163e8ae3d3be5cf
alexandervieira <alexander.silva@al.infnet.edu.br>
2019-01-12 01:12:53 -0200

    First commit - added examples of basic types in C #

---
+++ ConsoleApp_Training_CSharp/ConsoleApp_Training_CSharp/BasicTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Training_CSharp {
+	public static class BasicTypes {
+		public static bool complete = false;
+		public static char gender = 'F';
+		public static char letter = '\u0041';
+		public static byte n1 = 127;
+		public static int n2 = 1000;
+		public static int n3 = 2147483647;
+		public static long n4 = 2147483648L;
+		public static float n5 = 4.5f;
+		public static double n6 = 4.5;
+		public static string name = "Mary Green";
+		public static Object obj1 = "Alex Brown";
+		public static Object obj2 = 5.5f;
+	}
+}
---
+++ ConsoleApp_Training_CSharp/ConsoleApp_Training_CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp_Training_CSharp {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Example of basic types in C#: \n");
+			Console.WriteLine($"Variable bool: {BasicTypes.complete}");
+			Console.WriteLine($"Variable char: {BasicTypes.gender}");
+			Console.WriteLine($"Variable char: {BasicTypes.letter}");
+			Console.WriteLine($"Variable byte: {BasicTypes.n1}");
+			Console.WriteLine($"Variable int: {BasicTypes.n2}");
+			Console.WriteLine($"Variable int: {BasicTypes.n3}");
+			Console.WriteLine($"Variable long: {BasicTypes.n4}");
+			Console.WriteLine($"Variable float: {BasicTypes.n5}");
+			Console.WriteLine($"Variable double: {BasicTypes.n6}");
+			Console.WriteLine($"Variable string: {BasicTypes.name}");
+			Console.WriteLine($"Variable object: {BasicTypes.obj1}");
+			Console.WriteLine($"Variable object: {BasicTypes.obj2}");
+			Console.WriteLine("-------------------------------");
+		}
+	}
+}
