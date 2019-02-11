5a7afca83860c5245f15df7406f6511476cb1086
Alexander <asilva943rj@gmail.com>
2019-02-11 17:10:31 -0200

    Added Developer, Department, HourContract, and Enum classes.

---
+++ ConsoleApp_Training_CSharp/ConsoleApp_Training_CSharp/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp_Training_CSharp.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
